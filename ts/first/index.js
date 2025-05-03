5ea70338b0ce830993a24a3a1cf78db887d9f90d
Yogita <radheyavrushali2@gmail.com>
2025-05-03 19:51:24 +0530

    first commit

---
+++ ts/first/index.js
@@ -0,0 +1,5 @@
+var a = 20;
+// a="prasad";
+console.log(a);
+var x = "prasad";
+console.log(x, typeof x);
---
+++ ts/first/index.ts
@@ -0,0 +1,5 @@
+let a=20;
+// a="prasad";
+console.log(a);
+let x="prasad"
+console.log(x , typeof x);
